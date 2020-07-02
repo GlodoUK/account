76a83705cf75e3dfa94ed4177b17eae24d5d6c8a
Karl Southern <karl@theangryangel.co.uk>
2020-07-02 11:05:50 +0100

    Add some generic modules to enable queuing of account_move_line reconcile

---
+++ account_payment_better_matching_queued/wizards/account_payment_better_matching.py
@@ -0,0 +1,23 @@
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class AccountPaymentBetterMatching(models.TransientModel):
+    _inherit = 'account.payment.better.matching'
+
+    def process_queued(self):
+        if not self.partner_id:
+            raise UserError(_("Payments without a partner can't be matched"))
+
+        if not self.move_line_id:
+            raise UserError(_("No move needing to be reconcilced"))
+
+        if not self.matched_move_line_ids:
+            raise UserError(_("No moves selected for reconciliation"))
+
+        records = self.env['account.move.line']
+
+        records |= self.move_line_id
+        records |= self.matched_move_line_ids
+
+        records.with_delay().reconcile()
