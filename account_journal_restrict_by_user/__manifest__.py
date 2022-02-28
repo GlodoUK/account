8c430457b311a2a6b73ec9536141d1832d593b29
Karl Southern <karl@theangryangel.co.uk>
2022-02-28 17:06:32 +0000

    feat(account_journal_restrict_by_user): new module

---
+++ account_journal_restrict_by_user/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "account_journal_restrict_by_user",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "summary": """Account Journal Restriction by Users""",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "data/groups.xml",
+        "views/account.xml",
+    ],
+    "license": "Other proprietary",
+}
