e66eabac77acba1638f30f1500f656e2e5a0a9dc
Karl Southern <karl@theangryangel.co.uk>
2022-07-06 15:31:07 +0100

    GH100436

--- account_journal_restrict_by_user/models/sequence_mixin.py
+++ account_journal_restrict_by_user/models/sequence_mixin.py
@@ -4,5 +4,7 @@ from odoo import models
 class SequenceMixin(models.AbstractModel):
     _inherit = "sequence.mixin"
 
-    def _get_last_sequence(self, relaxed=False):
-        return super(SequenceMixin, self.sudo())._get_last_sequence(relaxed=relaxed)
+    def _get_last_sequence(self, relaxed=False, lock=True):
+        return super(SequenceMixin, self.sudo())._get_last_sequence(
+            relaxed=relaxed, lock=lock
+        )
