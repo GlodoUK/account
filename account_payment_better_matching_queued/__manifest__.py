020ecd0d1a91368bc7810de793423ab372947203
Edwin Blommaerts <edwin.blommaerts@gmail.com>
2022-08-10 16:28:39 +0100

    [fix] Change job from decorator to xml file

--- account_payment_better_matching_queued/__manifest__.py
+++ account_payment_better_matching_queued/__manifest__.py
@@ -10,6 +10,9 @@
         "account_payment_better_matching",
         "account_move_line_reconcile_queued",
     ],
-    "data": ["wizards/account_payment_better_matching.xml"],
+    "data": [
+        "wizards/account_payment_better_matching.xml",
+        "data/account_move_line_partial_reconcile_job.xml",
+    ],
     "demo": [],
 }
