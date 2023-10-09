f6b5477d6df43e2eb963d81892f080dcd1cf5f94
Karl Southern <karl@theangryangel.co.uk>
2023-10-09 17:29:55 +0100

    fix

--- credit_control/models/mail_activity.py
+++ credit_control/models/mail_activity.py
@@ -5,8 +5,10 @@ class MailActivity(models.Model):
     _inherit = "mail.activity"
 
     def _action_done(self, feedback=False, attachment_ids=None):
-        for activity in self:
-            record = self.env[activity.res_model_id.model].sudo().browse(activity.res_id)
+        for activity in self.sudo():
+            record = (
+                self.env[activity.res_model_id.model].sudo().browse(activity.res_id)
+            )
             if activity.res_model_id.model == "sale.order":
                 record.skip_credit_control_rules = True
         return super()._action_done(feedback, attachment_ids)
